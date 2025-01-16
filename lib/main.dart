6bb511ca294bfac6ffa8a4fe346c5fcaa617ca99
Michael Ivan <mic23756@gmail.com>
2025-01-16 02:21:30 +0100

    SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: QuestionScreen(),
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _startSplashScreen();
+  }
+
+  // écran de démarrage va a queqtionscreen après 5 secondes
+  _startSplashScreen() {
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const QuestionScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[800],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/images/icon.png',
+              height: 100,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Test de Personnalité",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
